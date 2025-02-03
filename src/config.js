4c89319a48ff39b59d78c7928c22cae15590ffe5
timkoopmans <tim.koops@gmail.com>
2025-02-03 16:13:06 +1100

    rename constant

--- src/config.js
+++ src/config.js
@@ -1,3 +1,11 @@
-export const config = {
-    onDemand: 10000, baseline: 10000, peak: 100000, peakWidth: 1, totalHoursPerMonth: 730
+export const cfg = {
+    onDemand: 10000,
+    baseline: 10000,
+    peak: 100000,
+    peakWidth: 1,
+    totalHoursPerMonth: 730,
+    pricePerRCU: 0.00065,
+    pricePerWCU: 0.00013,
+    pricePerRCU_IA: 0.00016,
+    pricePerWCU_IA: 0.00081,
 };
