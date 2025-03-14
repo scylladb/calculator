f84d341000de383c9edf6a31500bdad10d14abe8
timkoopmans <tim.koops@gmail.com>
2025-03-14 10:03:40 -0700

    show average monthly cost

--- src/calculator.js
+++ src/calculator.js
@@ -251,7 +251,8 @@ function logCosts() {
         `DAX: $${Math.floor(cfg.dynamoDaxCost).toLocaleString()}`,
         `---: ---`,
         `Total upfront cost: $${Math.floor(cfg.dynamoCostTotalUpfront).toLocaleString()}`,
-        `Total monthly cost: $${Math.floor(cfg.dynamoCostTotalMonthly).toLocaleString()}`]);
+        `Total monthly cost: $${Math.floor(cfg.dynamoCostTotalMonthly).toLocaleString()}`,
+        `Total monthly cost (averaged): $${Math.floor(cfg.dynamoCostTotalMonthlyAveraged).toLocaleString()}`]);
 
     updateSavedCosts(logs);
 }
