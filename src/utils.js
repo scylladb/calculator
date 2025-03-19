e961d760183fe68fd0bf4f8f89c1f18cb6fd675d
timkoopmans <tim.koops@gmail.com>
2025-03-19 21:32:05 +1100

    fix formatBytes

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,7 @@ export function formatNumber(num) {
 export function formatBytes(bytes) {
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
     if (bytes === 0) return '0 B';
-    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
     return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
 }
 
