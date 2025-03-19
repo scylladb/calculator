3046983c6067acecc311a633c1d79fbe3df239cc
timkoopmans <tim.koops@gmail.com>
2025-03-19 21:03:37 +1100

    add reserved unit test

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -117,3 +117,53 @@ describe('calculateProvisionedCosts', () => {
         expect(cfg.dynamoCostTotal.toFixed(2)).toBe("19700.00");
     });
 });
+
+describe('calculateReservedCosts', () => {
+    beforeEach(() => {
+        cfg.pricing = 'provisioned';
+        cfg.storageGB = 2048;
+        cfg.tableClass = 'standard';
+        cfg.itemSizeB = 1024;
+        cfg.baseline = 100_000;
+        cfg.peak = 200_000;
+        cfg.peakWidth = 3;
+        cfg.peakHours = cfg.peakWidth * 30;
+        cfg.hoursPerMonth = 730;
+        cfg.baselineHours = cfg.hoursPerMonth - cfg.peakHours;
+        cfg.readRatio = 0.80;
+        cfg.writeRatio = 0.20;
+        cfg.itemSizeKB = cfg.itemSizeB * (1 / 1024);
+        cfg.itemSizeKB = cfg.itemSizeKB > 1 ? Math.floor(cfg.itemSizeKB) : cfg.itemSizeKB;
+        cfg.readStronglyConsistent = 1;
+        cfg.readEventuallyConsistent = 0;
+        cfg.readTransactional = 0;
+        cfg.readNonTransactional = 1;
+        cfg.writeTransactional = 0;
+        cfg.writeNonTransactional = 1;
+        cfg.reserved = 100;
+        cfg.dynamoCostMonthlyRCU = 0;
+        cfg.dynamoCostUpfrontRCU = 0;
+        cfg.dynamoCostMonthlyWCU = 0;
+        cfg.dynamoCostUpfrontWCU = 0;
+        cfg.dynamoCostStorage = 0;
+        cfg.dynamoCostProvisionedMonthly = 0;
+        cfg.dynamoCostTotalMonthly = 0;
+        cfg.dynamoCostProvisionedUpfront = 0;
+    });
+
+    it('should calculate the correct reserved costs', () => {
+        calculateProvisionedCosts();
+        calculateStorageCost();
+
+        cfg.dynamoCostTotalMonthly = cfg.dynamoCostProvisionedMonthly + cfg.dynamoCostStorage;
+        cfg.dynamoCostTotalUpfront = cfg.dynamoCostProvisionedUpfront;
+
+        expect(cfg.dynamoCostStorage.toFixed(2)).toBe("512.00");
+        expect(cfg.dynamoCostMonthlyRCU.toFixed(2)).toBe( "2396.00");
+        expect(cfg.dynamoCostUpfrontRCU.toFixed(2)).toBe( "24000.00");
+        expect(cfg.dynamoCostMonthlyWCU.toFixed(2)).toBe( "3038.80");
+        expect(cfg.dynamoCostUpfrontWCU.toFixed(2)).toBe( "30000.00");
+        expect(cfg.dynamoCostTotalMonthly.toFixed(2)).toBe("5946.80");
+        expect(cfg.dynamoCostTotalUpfront.toFixed(2)).toBe("54000.00");
+    });
+});
